5f87d60f51886517dcfd28c29b01bd3c601c8eca
Evan Crook <evannc@gmail.com>
2017-02-02 22:43:11 -0800

    POST text, get back spaCy parse

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import spacy
+
+print "Loading spaCy ..."
+nlp = spacy.load('en')
+print "Loaded spaCy"
 
 server = Flask(__name__)
 
-@server.route("/")
-def hello():
-	return "Greetings. My name is Benedict."
+@server.route("/", methods=['POST'])
+def parse():
+	json_ = request.get_json(force=True)
+	print "Received JSON: ", json_
+	message = json_["message"]
+	doc = nlp(unicode(message))
+	return ' '.join([':'.join([w.text, w.tag_]) for w in doc])
 
 if __name__ == "__main__":
 	server.run()
